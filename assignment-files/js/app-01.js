9fc2e22d81a02f66e9ba0b2aa9391361908847e4
travis <t3patterson@gmail.com>
2018-05-22 12:01:52 -0500

    updating assignment files

--- assignment-files/js/app-01.js
+++ assignment-files/js/app-01.js
@@ -1,5 +1,7 @@
+/* 01 - UI TABS :: Complete AFTER variables */
+
 var membershipHTML = `
-  <h2>Join us Today!</h2>
+  <h2>Join Us Today!</h2>
   <p>
     You can be part of the Paley Center Community as an <em>Individual Member</em>, <em>Patron Member</em>,
     <em>Media Council Member</em> or <em>Corporate Member</em>. All include very special benefits for YOU
@@ -12,6 +14,7 @@ var membershipHTML = `
 `
 
 var programsHTML = `
+  <h2>Public Programs</h2>
   <ul>
     <li>PaleyLive LA</li>
     <li>PaleyLive NY</li>
@@ -19,6 +22,47 @@ var programsHTML = `
     <li>Patron Circle & President Circle</li>
   </ul>
 `
+
+var screeningsHTML = `
+  <h2>Join us Today!</h2>
+  <p>
+    Saturdays and Sundays in New York and Los Angeles.
+  </p>
+  <p>
+    12:00 to 4:00 local time • FREE ADMISSION.
+  </p>
+
+  <p>
+    <a href="#">Visit New York</a> | Visit Los Angeles
+  </p>
+
+  <p>
+    Weekend Screenings from the Paley Archive on the Big Screen in
+    NY & LA offer great family entertainment, PLUS programming
+    selected for our members and screened for the public. You haven't
+    really experienced TV until you've seen it on a movie screen with
+    fellow fans.
+  </p>
+`
+
+var pressHTML = `
+  <h2>Press Info</h2>
+  <hr/>
+  <h4>Current Press Release</h4>
+  <p><a href="#">Read More</a></p>
+  <hr/>
+  <h4>Press Release Archive</h4>
+  <p><a href="#">Read More</a></p>
+  <hr/>
+  <h4>Press Kit/Logos</h4>
+  <p>View and download press materials</p>
+  <p><a href="#">Read More</a></p>
+  <hr/>
+  <h4>Register for Press Information</h4>
+  <p><a href="#">Click to download and fill out form</a></p>
+  <hr/>
+`
+
 
 /* 01 - UI TABS :: Your code below.... */
 
