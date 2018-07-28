afe87afd1a5a4b5e38e37f1f53a11b882b3a002b
christian8aguirre <christian8.aguirre@icloud.com>
2018-07-28 18:45:47 -0500

    D20_lab--interactive-components

--- assignment-files/js/app-02.js
+++ assignment-files/js/app-02.js
@@ -1,13 +1,11 @@
 /* 02 - FORM VALIDATION :: YOUR CODE BELOW */
 
 var formulario = document.forms.formulario;
-console.log(formulario);
-
  formulario.addEventListener('submit', function (e) {
     e.preventDefault();
     var usernameTexBox = e.target.elements.username;
     if (usernameTexBox.value !== '') {
-      document.querySelector('.validation--username').textContent = 'Success!!!';
+        document.querySelector('.validation--username').textContent = 'Success!!!';
     } else{
         document.querySelector('.validation--username').textContent = 'User cannot be blank';  
     }
@@ -26,7 +24,25 @@ console.log(formulario);
         document.querySelector('.validation--confirm-password').textContent = 'Password must be longer than 8 characters & match original password';
     }
 
+    var taxidTextBox = e.target.elements.taxid;
+    if ((taxidTextBox.value * 1) > 0) {
+        document.querySelector('.validation--tax-id').textContent = 'Success!!!';
+    } else {
+        document.querySelector('.validation--tax-id').textContent = 'Input field must contain a number > 0';
+    }
+
+    var accountTextBox = e.target.elements.account;
+    if (accountTextBox.value === "") {
+        document.querySelector('.validation--account').textContent = 'You must select an Option';
+    }else {
+        document.querySelector('.validation--account').textContent = 'Success!!!';
+    }
     
-    
+    var termsofserviceCheckBox = e.target.elements.termsofservice;
+    if (termsofserviceCheckBox.checked === true) {
+        document.querySelector('.validation--terms-of-service').textContent = "Success!!!";
+    } else {
+        document.querySelector('.validation--terms-of-service').textContent = "You must check this box before proceding";
+    }
     
 });
